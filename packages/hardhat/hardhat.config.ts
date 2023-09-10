abb03ab46d1120d8728062446411645eb379e2ef
OwlWilderness <98717833+OwlWilderness@users.noreply.github.com>
2023-09-10 15:44:26 -0400

    add custom chains section for gnosis verification

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -9,11 +9,15 @@ import "@matterlabs/hardhat-zksync-verify";
 // If not set, it uses ours Alchemy's default API key.
 // You can get your own at https://dashboard.alchemyapi.io
 const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
+
+//tekhApiKey
+const tekhApiKey = process.env.TEKH_API_KEY;
 // If not set, it uses the hardhat account 0 private key.
 const deployerPrivateKey =
   process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 // If not set, it uses ours Etherscan default API key.
 const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
+const gnosisscanApiKey = process.env.GNOSISSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -91,11 +95,12 @@ const config: HardhatUserConfig = {
       verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
     },
     gnosis: {
-      url: "https://rpc.gnosischain.com",
+      url: `https://nethermind-nethermind-xdai.${tekhApiKey}.dyndns.dappnode.io`,
       accounts: [deployerPrivateKey],
     },
     chiado: {
       url: "https://rpc.chiadochain.net",
+      gasPrice: 1000000000,
       accounts: [deployerPrivateKey],
     },
     base: {
@@ -105,6 +110,33 @@ const config: HardhatUserConfig = {
     baseGoerli: {
       url: "https://goerli.base.org",
       accounts: [deployerPrivateKey],
+    },
+  },
+  etherscan: {
+    customChains: [
+      {
+        network: "chiado",
+        chainId: 10200,
+        urls: {
+          //Blockscout
+          apiURL: "https://gnosis-chiado.blockscout.com/api",
+          browserURL: "https://gnosis-chiado.blockscout.com/",
+        },
+      },
+      {
+        network: "gnosis",
+        chainId: 100,
+        urls: {
+          // Gnosisscan
+          apiURL: "https://api.gnosisscan.io/api",
+          browserURL: "https://gnosisscan.io/",
+        },
+      },
+    ],
+    apiKey: {
+      //blockscout explorer verification does not require keys
+      chiado: "not-needed",
+      gnosis: `${gnosisscanApiKey}`,
     },
   },
   verify: {
